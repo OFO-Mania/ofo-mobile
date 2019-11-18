afd3ac8a27f299f703beabde26befe33ce8177c6
danang-id <danang.galuh.t@gmail.com>
2019-11-18 19:49:07 +0700

    Implements: Sentry

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import AppNavigation from './src/navigation/AppNavigation';
+import { dsn } from './sentry.json';
+
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({ dsn });
 
 const App = () => {
 
