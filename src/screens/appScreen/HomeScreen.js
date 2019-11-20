12bfe919c79fe30ee8c2673f5045ecb182a5eeb7
Aldo Ignata Chandra <aldoignatachandra@gmail.com>
2019-11-20 16:40:41 +0700

    Update : Home Screen

--- src/screens/appScreen/HomeScreen.js
+++ src/screens/appScreen/HomeScreen.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react';
-import { Text, View, Image, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
+import { Dimensions,Text, View, Image, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 import styles from '../../styles/appScreen/StyleHeader';
 import stylesHome from '../../styles/appScreen/StyleHome';
-import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
 const HomeScreen = () => {
 
     const [cash, setash] = useState("0");
-    const [points, setPoints] = useState("6.900")
+    const [points, setPoints] = useState("6.900");
+    const [carousel, setCarousel] = useState(null);
+    const [bannerList, setBannerList] = useState([
+        { banner: require('../../assets/images/imagesHome/Deal1.jpg') },
+        { banner: require('../../assets/images/imagesHome/Deal2.jpg') },
+        { banner: require('../../assets/images/imagesHome/Deal3.jpg') },
+        { banner: require('../../assets/images/imagesHome/Deal4.jpg') },
+        { banner: require('../../assets/images/imagesHome/Deal5.jpg') },
+        { banner: require('../../assets/images/imagesHome/Deal6.jpg') }
+    ]);
+    const [activeSlide, setActiveSlide] = useState(0);
+
+    const renderBanner = ({item, index}) => {
+        return (
+            <View style={{
+                    alignSelf:"center",
+                    height:150,
+                    width:320, 
+                }}>
+                <Image 
+                    source={item.banner} 
+                    style={{height:"100%", width:"100%", borderRadius:15}}
+                />
+            </View>
+        );
+    }
 
     return (
         <>
@@ -128,9 +152,64 @@ const HomeScreen = () => {
                     </TouchableOpacity>
                 </View>
                 <View style={{width:"100%", height:10, backgroundColor:"#F8F8F8"}} />
-                <Text>Cashback Bikin Melek</Text>
+                <View style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center",marginHorizontal:15, marginTop:10, marginBottom:10}}>
+                    <Text style={{fontSize:20, fontWeight:"bold"}}>Cashback News</Text>
+                    <Text style={{fontSize:17, fontWeight:"bold", color:"#06B3BA"}}>See All</Text>
+                </View>
+                <Carousel
+                    ref={setCarousel}
+                    data={bannerList}
+                    renderItem={renderBanner}
+                    sliderWidth={Dimensions.get("window").width}
+                    itemWidth={Dimensions.get("window").width - 20}
+                    onSnapToItem={setActiveSlide}
+                />
+                <Pagination
+                    dotsLength={bannerList.length}
+                    activeDotIndex={activeSlide}
+                    // containerStyle={{}}
+                    dotStyle={{
+                        width: 13,
+                        height: 8,
+                        borderRadius: 5,
+                        marginHorizontal: -5,
+                        backgroundColor: '#06B3BA'
+                    }}
+                    inactiveDotStyle={{
+                        width: 8,
+                        height: 8,
+                        borderRadius: 5,
+                        marginHorizontal: -5,
+                        backgroundColor: 'black'
+                    }}
+                    inactiveDotOpacity={0.2}
+                    inactiveDotScale={1}
+                />
                 <View style={{width:"100%", height:10, backgroundColor:"#F8F8F8"}} />
-                <Text>Yang Menarik Di Ovo</Text>
+                <Text style={{fontWeight:"bold", fontSize:20, marginTop:15, marginLeft:15}}>Interesting In OFO</Text>
+                <Text style={{fontSize:15,marginHorizontal:15, opacity:0.3}}>Don't claim to update if you haven't tried this feature yet</Text>
+                <View style={{flex:1, flexDirection:"row", justifyContent:"center", marginTop:10, marginBottom:30}}>
+                    <View style={{width:"44%", height:220, marginHorizontal:5, borderRadius:12, borderWidth:0.1}}>
+                        <Image source={require('../../assets/images/imagesHome/ImageHelp.png')}
+                               style={{width:"100%", height:80, borderTopLeftRadius:12, borderTopRightRadius:12}}
+                        />
+                        <Text style={{fontWeight:"bold", marginHorizontal:15, marginTop:10, fontSize:15}}>Help Center</Text>
+                        <Text style={{marginHorizontal:15, marginTop:10, fontSize:12}}>
+                            Have any problems or questions related to OFO? You can send it here
+                        </Text>
+                        <Text style={{marginTop:10,alignSelf:"center", color:"#06B3BA", fontWeight:"bold"}}>OFO Help</Text>
+                    </View>
+                    <View style={{width:"44%", height:220, marginHorizontal:5, borderRadius:12, borderWidth:0.1}}>
+                        <Image source={require('../../assets/images/imagesHome/ImageInvest.png')}
+                               style={{width:"100%", height:80, borderTopLeftRadius:12, borderTopRightRadius:12}}
+                        />
+                        <Text style={{fontWeight:"bold", marginHorizontal:15, marginTop:10, fontSize:15}}>OFO Invest</Text>
+                        <Text style={{marginHorizontal:15, marginTop:10, fontSize:12}}>
+                            It's time to start investing early, for a better future
+                        </Text>
+                        <Text style={{marginTop:24,alignSelf:"center", color:"#06B3BA", fontWeight:"bold"}}>Start Invest</Text>
+                    </View> 
+                </View>
             </ScrollView>
         </>
     )
